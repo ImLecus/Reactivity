b33827b0c8abbaae074967193626cc3c793e74fc
ImLecus <marcosgf2005@gmail.com>
2023-03-10 18:35:35 +0100

    Added front pages

--- app/src/components/califications/Califications.jsx
+++ app/src/components/califications/Califications.jsx
@@ -1,12 +1,16 @@
-import { ScrollView, TouchableOpacity, Image } from "react-native"
+import { ScrollView, TouchableOpacity, Image, Text } from "react-native"
 import Cache from "../../data/cache.json"
 import Header from "../extras/Header"
 import Calification from "./Calification"
 import { icons } from "../../data/theme"
 import { Paragraph, Line } from "../basic/Components"
+import FrontPage from "../extras/FrontPage"
+import { useState } from "react"
 export default function Califications({navigation}){
+    const [open,setOpen] = useState(false)
     return(
     <>
+        <FrontPage open={open}/>
         <Header navigation={navigation} id={2} />
         <ScrollView style={{padding: 30, marginTop: 70}}>
             {
@@ -15,7 +19,7 @@ export default function Califications({navigation}){
                 ))
             }
             <Line w={"100%"} opacity={0.1}/>
-            <TouchableOpacity style={{flexDirection:"row", alignItems: "center", width: "100%", marginTop: 20}}>
+            <TouchableOpacity style={{flexDirection:"row", alignItems: "center", width: "100%", marginTop: 20}} onPress={() => {setOpen(true)}}>
                 <Image source={icons.add} style={{marginRight: 20, opacity: 0.6}}/>
                 <Paragraph text={"Nueva calificación..."}/>
             </TouchableOpacity>
---
+++ app/src/components/extras/FrontPage.jsx
@@ -0,0 +1,13 @@
+import { View, TouchableWithoutFeedback, Alert } from "react-native";
+import { theme } from "../../data/theme";
+import { useContext } from "react";
+import { ThemeContext } from "../UserContext";
+export default function FrontPage({open, w, h}){
+    const colorTheme = useContext(ThemeContext)
+    return(
+        <View style={{display: open? "flex": "none",position: "absolute", width:"100%", height:"100%", backgroundColor: "#00000011", zIndex: 100, justifyContent: "center", alignItems: "center"}}>
+            <View style={{width: w? w: "80%", height: h? h: "60%", backgroundColor: theme[colorTheme].bg, borderRadius: 10}}>
+            </View>
+        </View>
+    )
+}
